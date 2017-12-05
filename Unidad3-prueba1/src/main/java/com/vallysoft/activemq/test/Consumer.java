3994dc6630b03b3852773166d9316da3fca70e68
cerokool32 <cerokool32@gmail.com>
2017-12-04 20:57:45 -0500

    Prueba Hibernate

--- Unidad3-prueba1/src/main/java/com/vallysoft/activemq/test/Consumer.java
+++ Unidad3-prueba1/src/main/java/com/vallysoft/activemq/test/Consumer.java
@@ -22,7 +22,7 @@ public class Consumer {
 	// default broker URL is : tcp://localhost:61616"
 
 	// Name of the queue we will receive messages from
-	private static String subject = "VALLYSOFTQ";
+	private static String subject = "LDS";
 
 	public static void main(String[] args) throws JMSException {
 		// Getting JMS connection from the server
@@ -42,16 +42,21 @@ public class Consumer {
 		// Here we receive the message.
 		// By default this call is blocking, which means it will wait
 		// for a message to arrive on the queue.
-		Message message = consumer.receive();
-
+		Message message;
+		
+		while((message = consumer.receive())!= null) 
+		{
+			if (message instanceof TextMessage) {
+				TextMessage textMessage = (TextMessage) message;
+				System.out.println("Receivedage '" + textMessage.getText() + "'");
+			}
+		}
+	
 		// There are many types of Message and TextMessage
 		// is just one of them. Producer sent us a TextMessage
 		// so we must cast to it to get access to its .getText()
 		// method.
-		if (message instanceof TextMessage) {
-			TextMessage textMessage = (TextMessage) message;
-			System.out.println("Receivedage '" + textMessage.getText() + "'");
-		}
+		
 		connection.close();
 	}
 }
